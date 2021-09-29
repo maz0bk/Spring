c9eac169b697d65e0227adf7d9e3fc78d19224a1
Vladimir <maz0bk@gmail.com>
2021-09-29 18:14:12 +0300

    сделал 9 дз

---
+++ src/main/java/ru/gb/controllers/OrderController.java
@@ -0,0 +1,39 @@
+package ru.gb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.gb.entities.Order;
+import ru.gb.repositories.OrderRepository;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/order")
+public class OrderController {
+    OrderRepository orderRepository;
+
+    public OrderController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @GetMapping()
+    public String getForm(Model uiModel){
+        Order order = new Order();
+        uiModel.addAttribute("order", order);
+//        uiModel.addAttribute("testString", "string for test");
+        return "newOrder";
+    }
+
+    @PostMapping()
+    public String create( @ModelAttribute("order")Order order) {
+        orderRepository.addOrder(order);
+        return "newOrder";
+    }
+
+    @GetMapping("/all")
+    @ResponseBody
+    public List<Order> showAll(){
+        return orderRepository.getOrderList();
+    }
+}
